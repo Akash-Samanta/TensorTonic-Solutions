0807729675ed416ca02bdb950da7e3006f4c0435
Akash Samanta <akashsamanta.web@gmail.com>
2026-02-18 10:53:33 +0530

    Add solution for percent-change

---
+++ percent-change/percent-change.py
@@ -0,0 +1,9 @@
+def percent_change(series):
+    """
+    Compute the fractional change between consecutive values.
+    """
+    # Write code here
+    return [
+        (series[i] - series[i-1]) / series[i-1] if series[i-1]!= 0 else 0 
+        for i in range(1, len(series))
+    ]
